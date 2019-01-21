17b0dd831b813b8772471f1d9706da18add099e2
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2019-01-20 20:04:31 -0500

    Improvements

--- textadept/.textadept/init.lua
+++ textadept/.textadept/init.lua
@@ -16,9 +16,8 @@ if not CURSES then
   rgb_dialog = require("rgb_dialog").dialog
 end
 
-function 	directory ()
-    return buffer.filename:match("^(.-)[^/]*$")
-end
+open_file_or_new = require("open_file_or_new").open_file_or_new
+keys.co = open_file_or_new
 
 -- Load extra snippets to the Lua snippets table; 
 -- make sure that the Lua lexer is loaded _first_.
@@ -79,58 +78,3 @@ buffer.indentation_guides = buffer.IV_NONE
 -- Disable code folding and hide the fold margin
 buffer.property.fold = "0"
 buffer.margin_width_n[2] = 0
-
--- TO BE PUT IN A SEPARATE MODULE, AND BOUND TO A KEY.
--- Disallow ./ and ../, since they make no practical sense when they appear in absolute filenames.
--- We might want a button that says, "Up", tbc.
-function open_or_create_file (dir)
-	local dir = dir or os.getenv("HOME") .. "/"
-	local dir_entries = {}
-	local gen_input = io.popen(string.format("ls -Fa %s", dir))
-	
-	for entry in gen_input:lines() do
-		if entry ~= "./" and entry ~= "../" then -- for now, disallow this - maybe add buttons that navigate.
-			dir_entries[#dir_entries + 1] = entry
-		end
-	end
-
-	
-	local button_label, dir_entry = ui.dialogs.filteredlist {
-		button1 = "Descend",
-		button2 = "Create",
-		title = "Open/New File", 
-		columns = {string.format("Contents of %s", dir)},
-		string_output = true,
-		items = dir_entries
-	}
-	
-	-- Create a new buffer with the desired name.
-	local function create ()
-		local button, filename = ui.dialogs.standard_inputbox {
-			title = "Create New File",
-			informative_text = "Textadept will create a new file with this name.",
-			text = dir
-		}
-		
-		if button == 1 then
-			buffer:new() -- make a new buffer, and switch focus to it.
-			io.save_file_as(filename) -- write the buffer to disk with the given absolute filename.
-		end
-	end
-
-	if button_label == "Descend" then
-		if not dir_entry then -- user input was not among the possible selections.
-			create() -- hence, prompt the user to create and open a new file here.
-		else
-			local entry_abspath = dir .. dir_entry -- otherwise, get the absolute path of the current selection.
-		
-			if dir_entry:find("/$") then -- the current selection is a directory.
-				example2(entry_abspath)
-			else
-				io.open_file(entry_abspath) -- we can open an existing, non-directory entry in Textadept.
-			end
-		end
-	elseif button_label == "Create" then
-		create()
-	end
-end
