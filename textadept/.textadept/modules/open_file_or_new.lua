c18e129f5c742b08f9b21b1eaaff2e43fbfc8ba0
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2019-01-22 06:30:03 -0500

    Fixed for when current buffer isn't visiting a file; streamlined documentation

--- textadept/.textadept/modules/open_file_or_new.lua
+++ textadept/.textadept/modules/open_file_or_new.lua
@@ -1,51 +1,51 @@
 --[[
 	README
-	
-	A module for opening files in Textadept, that uses a filtered list box that uses the output 
-of an 'ls' command. 
-	The button "Create" prompts to create a new file in the currently listed directory;
-the "Descend" button will either list a subdirectory, or open a file (depending on what
-the entry is.) Of course, also typing "Enter" on a selected entry will _descend_ into that entry.
+
+	A module for opening files in Textadept, that uses a filtered
+	list box that uses the output
+of an 'ls' command.
+	The button "Create" prompts to create a new file in the currently
+	listed directory;
+the "Descend" button will either list a subdirectory, or open a file
+(depending on what the entry is.) Of course, also typing "Enter" on a
+selected entry will _descend_ into that entry.
 	Press "Esc" to cancel the box.
-	
+
 	Quick review of the ls flags used here:
-	
+
 	-L
 		Dereference symbolic links;
 	-a
 		Show hidden files;
-	-p  
+	-p
 		Print directories with a final '/' (frontslash).
 	
-	By far, L and p are the most important flags; an entry is a directory if and only if -p gives it
-a final '/', so we can uniquely identify directories using this technique. Furthermore, L will dereference
-symbolic links; otherwise, it won't be able to tell apart a _symlink to a directory_ from a non-directory.
-Finally, -a gives us in addition all files that begin with a dot: this gives us access to "../", which allows
-us to move up a directory; and the box's filtering functionality eases the burden of wading through a ton
-of hidden files, making a full directory listing worth it.
-
-	Admittedly, the only real extra benefit this module provides is fuzzy search. I mistakenly thought that
-the default file opener in Textadept didn't let you create a new file, but that isn't true!
+	An entry is a directory if and only if -p gives it
+a final '/', so we can uniquely identify directories using this
+technique. Furthermore, L helps us tell apart a _symlink to a directory_
+from a non-directory. Finally, -a shows all hidden entries: this gives
+us access to "./" (creates a new file) and "../", which allows us to
+move up a directory; and the box's filtering functionality eases the
+burden of wading through a ton of hidden files, making a full directory
+listing worth it.
 ]]
 
 local M = {} 
 
 function M.open_file_or_new (dir)
-	local buffer_curdir = buffer.filename:match("^.*/")
-	local dir = dir or buffer_curdir
-	
+	local curdir = buffer.filename and 
+						buffer.filename:match("^.*/") or 
+						string.format("%s/", os.getenv("HOME"))
+						
+	local dir = dir or curdir
 
-	-- Our function only knows something is a directory because of a final '/' in its name.
 	local gen_input = io.popen(string.format("ls -Lap %s", dir))
 	local dir_entries = {}
-	
-	gen_input:read() -- throw out "./" (the reference to the current directory.)
 	
 	for entry in gen_input:lines() do
 		dir_entries[#dir_entries + 1] = entry
 	end
 
-	
 	local button_label, dir_entry = ui.dialogs.filteredlist {
 		button1 = "Descend",
 		button2 = "Create",
@@ -73,8 +73,8 @@ function M.open_file_or_new (dir)
 		if not dir_entry then -- user input was not among the possible selections.
 			alert(nil, "That file or directory doesn't exist.")
 			M.open_file_or_new(dir)
-	--	elseif dir_entry == "./" then -- if we want, file creation can be buttonless.
-	--		create() 
+		elseif dir_entry == "./" then -- cheat code for file creation.
+			create() 
 		elseif dir_entry == "../" then
 			local optional_leaf_entry  = "/[^/]*/?$" -- handles case where dir == "/".
 			local parent_dir = dir:sub(1, dir:find(optional_leaf_entry)) -- grab until where that entry starts.
