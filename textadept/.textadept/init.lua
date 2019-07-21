6beed95544ff23fc9f7f0fd63c52995151f08bf0
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2019-07-21 09:33:46 -0400

    Updates

--- textadept/.textadept/init.lua
+++ textadept/.textadept/init.lua
@@ -1,5 +1,3 @@
-require "theme"
-
 -- Pressing C-z will undo, instead.
 events.connect(events.SUSPEND, function()
 	buffer:undo()
@@ -19,74 +17,35 @@ keys.cmv.q = function ()
 end
 
 local modules = {
-	"theme",
-	"copy_paste",
 	"utils",
-	"ringbuffer",
+	"copy_paste",
 	"config",
 	"view_modal",
 }
 
-Log = require("log"):new(_USERHOME .. "/log.txt")
+-- Theming has to be done before initialization is complete.
+require "theme"
 
-for _, mod in ipairs(modules) do
-	local status, result = pcall(require, mod)
-
-	if not status then
-		Log:log(result, false)
-		goto end_of_config
-	end
+events.connect(events.INITIALIZED, function ()
+	local report
 	
-	if _G[mod] ~= nil then
-		Log:log("Name '%s' already taken.", false, mod)
-		goto end_of_config
-	end
+	for _, mod in ipairs(modules) do
+		local status, result = pcall(require, mod)
+
+		if not status then -- we all know what this means. :)
+			report = string.format("Error in module '%s':\n%s\nAborting config.", mod, result)
+			break
+		end
 		
-	_G[mod] = result
-end
-
--- Confirm that all went well with loading user modules.
-Log:log("Success.", true)
-
-::end_of_config::
-Log:finish()
-
-
-past_commands = ringbuffer:new(10)
-
--- Save the original command-entry launcher hook.
-local old_command = keys.lua_command["\n"] 
-
-keys.lua_command["\n"] = function ()
-	past_commands:push(ui.command_entry:get_text())
-	return old_command() 
-end
-
-
--- Save 'past_commands' after successive resets!
-events.connect(events.RESET_BEFORE, function (t)
-	t.past_commands = past_commands
-end)
-
-events.connect(events.RESET_AFTER, function (t)
-	past_commands = t.past_commands
-end)
-
-function select_command ()
-	local options = {
-		title = "Previous Commands",
-		columns = {"Which one?"},
-		string_output = true,
-		items = past_commands,
-	}
-	
-	local button, choice = ui.dialogs.filteredlist(options)
-	if button == "OK" then
-		ui.command_entry:set_text(choice)
-		ui.command_entry.finish_mode(function() old_command() end) -- telegraphic;
-		-- but I see a module in my future, tbc.
-		-- also, remove duplicates from past_commands.
-		-- Dependencies... tbc.
+		-- Check the availability of the module name, before making it global.
+		if _G[mod] ~= nil then
+			report = string.format("Name '%s' already taken.", mod)
+			break
+		end
+			
+		_G[mod] = result
 	end
-end
 	
+	local NOTIFY_STATUS = report and ui.print or utils.alert
+	NOTIFY_STATUS(report or "Success.")
+end)
